6c0fa83c3acd4d67404dbf40fa87a46dceba4210
sugar <qxsugar@gmail.com>
2023-09-18 19:18:35 +0800

    add wedding

--- wedding_src/vue.config.js
+++ wedding_src/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     // publicPath:process.env.NODE_ENV === 'production' ? '/vue_workspac/aihuhuproject/' : '/',
 
     //基本路径
-    baseUrl: './',//默认的'/'是绝对路径，如果不确定在根路径，改成相对路径'./'
+    publicPath: './',//默认的'/'是绝对路径，如果不确定在根路径，改成相对路径'./'
     // 输出文件目录
     outputDir: 'dist',
     assetsDir: 'static',
